64b2378d02b3e62502976058958a445e9d09f611
Joey Chow <39481847+Reown@users.noreply.github.com>
2021-07-10 23:43:12 +0800

    parse experiments

---
+++ fe/testscript.js
@@ -0,0 +1,8 @@
+function abc(){
+    document.getElementById("demo").innerHTML = "test";
+ };
+
+function parse(){
+    var x = document.getElementById("myTextarea").value;
+    document.getElementById("parse").innerHTML = x + 1;
+}
