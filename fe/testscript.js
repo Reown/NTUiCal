7d9b9203c0247904a43cd1188f510bbe28c958dd
Joey Chow <39481847+Reown@users.noreply.github.com>
2021-07-11 03:22:11 +0800

    removed

--- fe/testscript.js
+++
@@ -1,8 +0,0 @@
-function abc(){
-    document.getElementById("demo").innerHTML = "test";
- };
-
-function parse(){
-    var x = document.getElementById("myTextarea").value;
-    document.getElementById("parse").innerHTML = x + 1;
-}
