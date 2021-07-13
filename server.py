5ebe33bc8511f133395277a66e5870a63b6c561c
Joey Chow <39481847+Reown@users.noreply.github.com>
2021-07-14 02:45:16 +0800

    removed port

--- server.py
+++ server.py
@@ -18,5 +18,4 @@ def parse():
 def calview():
     return render_template('calview.html')
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(debug=True, port=port)
+  app.run(debug=True)
