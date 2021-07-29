4149baf14175b1520cfa409c36d91e0dee649037
Joey Chow <39481847+Reown@users.noreply.github.com>
2021-07-30 04:24:30 +0800

    added html support

--- fes/scrap.py
+++ fes/scrap.py
@@ -2,50 +2,42 @@ import codecs
 from bs4 import BeautifulSoup
 from helper import *
 
-raw_html = codecs.open("test.html", 'r').read()
-soup = BeautifulSoup(raw_html, features="html.parser")
+def fh(path):
 
-table = soup.find("table", attrs={"border": ""})
-table_data = table.tbody.find_all("tr")
+    raw_html = codecs.open(path, 'r').read()
+    soup = BeautifulSoup(raw_html, features="html.parser")
 
-course = []
-title = []
-ctype = []
-group = []
-day = []
-time = []
-venue = []
-weeks = []
+    table = soup.find("table", attrs={"border": ""})
+    table_data = table.tbody.find_all("tr")
 
-for i in range(1, len(table_data)):
+    course = []
+    title = []
+    ctype = []
+    group = []
+    day = []
+    time = []
+    venue = []
+    weeks = []
 
-    temprow = []
-    for td in table_data[i].find_all("td"):
-        temprow.append(td.text.replace('\n', ' ').strip())
+    for i in range(1, len(table_data)):
 
-    course.append(temprow[0])
-    title.append(temprow[1])
-    ctype.append(temprow[9])
-    group.append(temprow[10])
-    day.append(temprow[11])
-    time.append(temprow[12])
-    venue.append(temprow[13])
-    weeks.append(temprow[14].split(" ")[1])
+        temprow = []
+        for td in table_data[i].find_all("td"):
+            temprow.append(td.text.replace('\n', ' ').strip())
 
-topop = checkdel(weeks)
-course, title, ctype, group, day, time, venue, weeks = popall(course, title, ctype, group, day, time, venue, weeks, topop)
-timestart, timeend = cleantime(time)
-allweeks = cleanweeks(weeks)
-utilday = cleanday(day)
+        course.append(temprow[0])
+        title.append(temprow[1])
+        ctype.append(temprow[9])
+        group.append(temprow[10])
+        day.append(temprow[11])
+        time.append(temprow[12])
+        venue.append(temprow[13])
+        weeks.append(temprow[14].split(" ")[1])
 
-'''
-print(course)
-print(title)
-print(ctype)
-print(group)
-print(utilday)
-print(timestart)
-print(timeend)
-print(venue)
-print(allweeks)
-'''
+    topop = checkdel(weeks)
+    course, title, ctype, group, day, time, venue, weeks = popall(course, title, ctype, group, day, time, venue, weeks, topop)
+    timestart, timeend = cleantime(time)
+    allweeks = cleanweeks(weeks)
+    utilday = cleanday(day)
+
+    return course, title, ctype, group, utilday, timestart, timeend, venue, allweeks
